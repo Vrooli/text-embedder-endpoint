32b4129b2290b5b12366bed52cb6f3e8aa494b30
MattHalloran <matthalloran8@gmail.com>
2023-05-10 17:41:53 -0400

    Redis caching
    Updated to store calculated embeddings in Redis, so they don't have to be calculated again

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 import os
+import hashlib
+import json
 import logging
 from flask import Flask, request, jsonify
 from InstructorEmbedding import INSTRUCTOR
 from dotenv import load_dotenv
+import redis
 
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s')
@@ -11,6 +14,9 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 app = Flask(__name__)
+
+# Connect to Redis
+r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 try:
     # Load the model
@@ -26,11 +32,28 @@ def embed():
     instruction = request.json.get('instruction')
     sentence = request.json.get('sentence')
 
-    # Get the embeddings
-    embeddings = model.encode([[instruction, sentence]])
+    # Create a hash from the input
+    input_hash = hashlib.sha256(f"{instruction}{sentence}".encode()).hexdigest()
 
-    # Convert tensor to list and return
-    return jsonify(embeddings.tolist())
+    # Check if Redis has a key with that hash
+    if r.exists(input_hash):
+        # If so, use that data
+        embeddings = json.loads(r.get(input_hash))
+    else:
+        # If not, compute the embeddings
+        embeddings = model.encode([[instruction, sentence]])
+
+        # Convert tensor to list
+        embeddings = embeddings.tolist()
+
+        # Store the embeddings in Redis
+        r.set(input_hash, json.dumps(embeddings))
+
+        # Set a reasonable expiry time if you want to delete old data
+        # Here, for example, 24 hours (86400 seconds)
+        r.expire(input_hash, 86400)
+
+    return jsonify(embeddings)
 
 try:
     port = os.environ['PORT_EMBEDDINGS']
