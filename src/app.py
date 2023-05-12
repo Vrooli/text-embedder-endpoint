825137a2dd50ce80d3c024200c5fe3f96e04eea2
MattHalloran <matthalloran8@gmail.com>
2023-05-11 23:17:13 -0400

    Performance logs
    - Added logs for checking the performance of the embedder
    - Added limit to the number of sentences that can be processed at a time

--- src/app.py
+++ src/app.py
@@ -6,8 +6,12 @@ from flask import Flask, request, jsonify, abort
 from InstructorEmbedding import INSTRUCTOR
 from dotenv import load_dotenv
 import redis
+import time
 
-# Configure logging
+# Maximum number of sentences that can be processed at once
+MAX_SENTENCES = 100
+
+# Configure logging. Set to DEBUG for more verbose logging. Default is INFO.
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger = logging.getLogger(__name__)
 
@@ -30,40 +34,61 @@ logger.info('Model loaded!')
 # If API_KEY environment variable is set, require it in the request headers
 @app.before_request
 def check_api_key():
-    api_key = request.headers.get('key')
-    if os.environ.get('API_KEY'):
+    api_key = request.headers.get('key', '')
+    if os.environ.get('API_KEY', ''):
         if not api_key:
             abort(401, 'API Key required')
-        elif not api_key == os.environ.get('API_KEY'):
+        elif not api_key == os.environ.get('API_KEY', ''):
             abort(401, 'Invalid API Key')
+
+import time  # Add this import at the top of your file
 
 @app.route('/', methods=['POST'])
 def embed():
-    instruction = request.json.get('instruction')
-    sentences = request.json.get('sentences')
+    try:
+        start_time = time.time()  # Record the start time
 
-    all_embeddings = []
-    for sentence in sentences:
-        # Create a hash from the input
-        input_hash = hashlib.sha256(f"{instruction}{sentence}".encode()).hexdigest()
+        instruction = request.json.get('instruction')
+        sentences = request.json.get('sentences')
 
-        # Check if Redis has a key with that hash
-        if r.exists(input_hash):
-            # If so, use that data
-            embeddings = json.loads(r.get(input_hash))
-        else:
-            # If not, compute the embeddings
-            embeddings = model.encode([[instruction, sentence]])
+        # Check if the number of sentences exceeds the limit
+        if len(sentences) > MAX_SENTENCES:
+            return jsonify({"error": f"Too many sentences. The maximum is {MAX_SENTENCES}."}), 400
 
-            # Convert tensor to list
-            embeddings = embeddings.tolist()
+        all_embeddings = []
+        for sentence in sentences:
+            input_start_time = time.time()
 
-            # Store the embeddings in Redis
-            r.set(input_hash, json.dumps(embeddings))
+            # Create a hash from the input
+            input_hash = hashlib.sha256(f"{instruction}{sentence}".encode()).hexdigest()
 
-        all_embeddings.extend(embeddings)
+            # Check if Redis has a key with that hash
+            if r.exists(input_hash):
+                # If so, use that data
+                embeddings = json.loads(r.get(input_hash))
+            else:
+                # If not, compute the embeddings
+                embeddings = model.encode([[instruction, sentence]])
 
-    return jsonify({"embeddings": all_embeddings, "model": "instructor-base"})
+                # Convert tensor to list
+                embeddings = embeddings.tolist()
+
+                # Store the embeddings in Redis
+                r.set(input_hash, json.dumps(embeddings))
+
+            input_end_time = time.time()
+            logger.debug(f"Time for processing input '{sentence}': {input_end_time - input_start_time} seconds")
+
+            all_embeddings.extend(embeddings)
+
+        end_time = time.time()  # Record the end time
+        logger.debug(f"Total time for embed function: {end_time - start_time} seconds")
+
+        return jsonify({"embeddings": all_embeddings, "model": "instructor-base"})
+    except Exception as e:
+        logger.error(f"Error in embed function: {e}")
+        return jsonify({"error": "An error occurred during embedding. Please check your input and try again."}), 500
+
 
 @app.route('/help', methods=['GET'])
 def help():
